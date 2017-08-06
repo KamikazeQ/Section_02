91ca8bae55ad5023c50cd00dc529987f8abc9fcb
Quinton Gordon <quinton.gordon@gmail.com>
2017-08-06 11:54:27 -0400

    Section 18 completed

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -4,6 +4,9 @@ using namespace std;
 
 int main()
 {
+	constexpr int WORLD_LENGTH = 5;
+
 	cout << "Welcome to Bulls and Cows!" << endl;
+	cout << "Can you guess the " << WORLD_LENGTH << " letter isogram I'm thinking of?" << endl;
 	return 0;
 }
